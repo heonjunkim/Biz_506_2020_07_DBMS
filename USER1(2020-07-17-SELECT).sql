1d1892f9c5083abc020153c72d233979828c4bff
callor <callor@callor.com>
2020-07-17 16:47:07 +0900

    FK

--- USER1(2020-07-17-SELECT).sql
+++ USER1(2020-07-17-SELECT).sql
@@ -108,16 +108,180 @@ SELECT * FROM tbl_student WHERE st_num = '20020';
 -- 학생정봐 함께 성적정보 칼럼의 값들이 1개라도 (null)이 아닌 것으로 나타나면
 --      이 학생의 성적정보는 등록된 것이다.
 --      따라서 학생의 성적정보를 삭제해야 한다.
-
 SELECT *
 FROM tbl_student ST
     LEFT JOIN tbl_score SC
         ON ST.st_num = Sc.sc_num
 WHERE ST.st_num = '20020';        
 
+-- sc_num 데이터에 없는 조건을 부여하면
+-- DELETE를 수행하지 않을뿐 오류가 나거나 하지는 않는다.
+DELETE FROM tbl_score 
+WHERE sc_num = '20020';
+ROLLBACK;
+
+-- 성적데이터의 국어점수가 가장높은 값과 가장 낮은값은 무엇인가?
+SELECT MAX(sc_kor), MIN(sc_kor)
+FROM tbl_score;
+
+INSERT INTO tbl_score(sc_num, sc_kor) VALUES('20101',49) ;
 
 
+-- 최고점수는 100점, 최저점수는 50이다
+-- 이점수를 받은 학생은 누구일까
+SELECT SC.sc_num, ST.st_name, SC.sc_kor
+FROM tbl_score SC
+    LEFT JOIN tbl_student ST
+        ON SC.sc_num = ST.st_num
+WHERE SC.sc_kor = 100 OR SC.sc_kor = 50;
 
+--------------------------------------------
+-- sub query
+--------------------------------------------
+-- 두번이상 SELECT 수행해서 결과를 만들어야 하는경우
+-- 첫번째 SELECT 결과를 두번째 SELECT에 주입하여
+-- 동시에 두번이상의 SELECT를 수행하는 방법
+-- sub query는 JOIN로 모두 구현가능하다.
+-- 하지만 간단한 동작을 요구할때는 sub query를 사용하는 것이
+-- 쉬운방법이기도 하다.
+-- 또한 오라클 관련 정보들(구글링)중에 JOIN보다는 sub query를
+-- 사용한 예제들이 많아서 코딩에 다소 유리한면도 있다.
+
+-- sub query를 사용하게 되면 SELECT 여러번 실행되기 때문에
+-- 약간만 코딩이 변경이 되어도 상당히 느린 실행결과를 낳게 된다.
+
+
+-- WHERE 절에서 SubQuery사용하기
+-- WHERE 처음에 칼럼명이 오고 (<=>) 
+--      오른족에 괄호를 포함한 SELEC 쿼리가 나와야 한다.
+-- sub query로 작동되는 SELECT 문은 기본적으로 1개의 결과만 나와야한다.
+-- sub query의해 연산된 결과의 값을 기준으로 칼럼에 조건문을 부여하는 방식
+-- sub query는 method, 함수를 호출하는 것과 같이 sub query가 return해주는 값을
+-- 칼럼과 비교하여 최종 결과물을 낸다.
+SELECT SC.sc_num, ST.st_name, SC.sc_kor
+FROM tbl_score SC
+    LEFT JOIN tbl_student ST
+        ON SC.sc_num = ST.st_num
+WHERE SC.sc_kor = 
+    (
+        SELECT MAX(sc_kor) FROM tbl_score
+    )
+OR SC.sc_kor = 
+    (
+        SELECT MIN(sc_kor) FROM tbl_score
+    );
+
+-- 국어점수의 평균을 구하고
+-- 평균점수보다 높은 점수를 얻은 학생들의 리스트를 구하고 싶다.
+
+SELECT AVG(sc_kor) FROM tbl_score;
+
+SELECT *
+FROM tbl_score
+WHERE sc_kor >= 74.54 ;
+
+SELECT *
+FROM tbl_score
+WHERE sc_kor >=
+(
+    SELECT AVG(sc_kor) FROM tbl_Score
+);
+
+
+-- 각 학생의 점수 평균을 구하고
+-- 전체학생의 평균을 구하여
+-- 각 학생의 평균점수가 전체학생의 평균점수보다 
+--      높은 리스트를 조회하시오
+DELETE FROM tbl_score where sc_num = '20101';
+
+SELECT sc_num, (sc_kor + sc_eng + sc_math + sc_music + sc_art) / 5 AS 평균
+FROM tbl_score;
+
+SELECT AVG((sc_kor + sc_eng + sc_math + sc_music + sc_art) / 5) AS 평균
+FROM tbl_score;
+
+-- SELECT Query 문이 실행되는 순서
+-- 1. FROM 절이 실행되어 tbl_score 테이블의 정보(칼럼정보) 를 가져오기
+-- 2. WHERE 절이 실행되어서 실제 가져올 데이터를 선별
+-- 3. GROUPY BY 이 실행되어 중복된 데이터를 묶어서 하나로 만든다.
+-- 4. SELECT 에 나열된 칼럼에 값을 채워넣고,
+-- 5. SELECT 에 정의된 수식을 연산, 결과를 보일준비
+-- 6. ORDER BY는 모든 쿼리가 실행되고 가장 마지막에 연산 수행되어 정렬을 한다.
+
+-- WHERE 절과 GROUPY BY 절에서는 Alias로 설정된 칼럼 이름을 사용할 수 없다.
+-- ORDER BY 에서는 Alias로 설정된 칼럼 이름을 사용할 수 있다.
+SELECT sc_num, (sc_kor + sc_eng + sc_math + sc_music + sc_art) / 5 AS 평균
+FROM tbl_score
+WHERE (sc_kor + sc_eng + sc_math + sc_music + sc_art) / 5  >=
+(
+    SELECT AVG((sc_kor + sc_eng + sc_math + sc_music + sc_art) / 5)
+    FROM tbl_score
+);
+
+-- 위의 Query를 활용하여
+-- 평균을 구하는 조건은 그대로 유지하고
+-- 학번이 20020 이전의 학생들만 추출하기
+SELECT sc_num, (sc_kor + sc_eng + sc_math + sc_music + sc_art) / 5 AS 평균
+FROM tbl_score
+WHERE (sc_kor + sc_eng + sc_math + sc_music + sc_art) / 5  >=
+(
+    SELECT AVG((sc_kor + sc_eng + sc_math + sc_music + sc_art) / 5)
+    FROM tbl_score
+) AND sc_num < '20020';
+
+-- 성적테이블에서 학번의 문자열을 자르기 수행하여
+-- 반명칭만 추출하기
+SELECT SUBSTR(sc_num,1,4) AS 반
+FROM tbl_score
+GROUP BY SUBSTR(sc_num,1,4)
+ORDER BY 반;
+
+-- 추출된 반명칭이 '2006' 보다 작은 값을 갖는 반만 추출
+-- HAVING : 성질이 WHERE 매우비슷하다.
+--      하는일이 GROUP BY 묶이거나, 통계함수로 생성값을 대상으로
+--      WHERE 연산을 수행하는 키워드
+SELECT SUBSTR(sc_num,1,4) AS 반
+FROM tbl_score
+GROUP BY SUBSTR(sc_num,1,4)
+HAVING SUBSTR(sc_num,1,4) < '2006'
+ORDER BY 반;
+
+-- 각반의 평균을 구하는 코드
+SELECT SUBSTR(sc_num,1,4) AS 반, ROUND(AVG((sc_kor+sc_eng+sc_math)/3)) 반평균
+FROM tbl_score
+GROUP BY SUBSTR(sc_num,1,4)
+HAVING ROUND(AVG((sc_kor+sc_eng+sc_math)/3)) >= 
+(
+    SELECT ROUND(AVG((sc_kor+sc_eng+sc_math)/3)) FROM tbl_score
+)
+ORDER BY 반;
+
+
+-- 2000 ~ 2005까지는 A 그룹, 2006 ~ 2010까지는 B 그룹일때
+-- 반명이 '2005' 이하, A 그룹의 반들 평균 구하기
+SELECT SUBSTR(sc_num,1,4) AS 반, ROUND(AVG((sc_kor+sc_eng+sc_math)/3)) 반평균
+FROM tbl_score
+GROUP BY SUBSTR(sc_num,1,4)
+HAVING SUBSTR(sc_num,1,4) <= '2005'
+ORDER BY 반;
+
+-- HAVING과 WHERE
+-- 두가지 모두 결과를 SELECTION하는 조건문을 설정하는 방식이다
+-- HAVING은 그룹으로 묶이거나 통계함수로 연산된 결과를 조건으로 설정하는 방식이고
+-- WHERE 아무런 연산이 수행되기 전에 원본 데이터를 조건으로 제한하는 방식이다.
+-- 어쩔수없이 통계결과를 제한할때는 HAVING을 써야한다.
+-- WHERE절에 조건을 설정하여 데이터를 제한한 후 연산을 수행할수 있다면
+-- 항상 그 방법을 우선 조건으로 설정하자
+-- HAVING WHERE 조건이 없으면 전체데이터를 상대로 상당한 연산을 수행한 후
+-- 조건을 설정하므로 상대적으로 WHERE 조건을 설정하는 것 보다 느리다.
+SELECT SUBSTR(sc_num,1,4) AS 반, ROUND(AVG((sc_kor+sc_eng+sc_math)/3)) 반평균
+FROM tbl_score
+WHERE SUBSTR(sc_num,1,4) <= '2005'
+GROUP BY SUBSTR(sc_num,1,4)
+ORDER BY 반;
+
+ID : m506
+PW : 0627204800
 
 
 
